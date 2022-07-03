1fa642e74ed3f2c3a896680665473b436a1ddef4
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-07-03 19:58:10 -0300

    Listing pessoa and adding pessoa components added

---
+++ src/app/pessoa/listar-pessoa/listar-pessoa.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Pessoa } from 'src/app/shared/models/pessoa.model';
+import { PessoaService } from '../services/pessoa.service';
+
+@Component({
+  selector: 'app-listar-pessoa',
+  templateUrl: './listar-pessoa.component.html',
+  styleUrls: ['./listar-pessoa.component.css']
+})
+export class ListarPessoaComponent implements OnInit {
+
+  pessoas: Pessoa[] = [];
+
+  constructor(private pessoaService : PessoaService) { }
+
+  ngOnInit(): void {
+    this.pessoas = this.listarTodos();
+  }
+
+  listarTodos(): Pessoa[]{
+    return this.pessoaService.listarTodos();
+
+    // return [
+    //   new Pessoa(1, "Razer", 20),
+    //   new Pessoa(2, "Brunna", 52),
+    //   new Pessoa(3, "Guilherme", 33),
+    //   new Pessoa(4, "Juan", 88),
+    // ];
+  }
+
+}
